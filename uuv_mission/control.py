a1b6ab79d42c64a3c1bb6d8e6340d3c0e660cc12
= <=>
2024-10-16 16:02:45 +0100

    removing unneccesary imports

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,6 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
 def PD_controller(reference_t1,depth_t1,e_t0,Kp,Kd):
     # Calculates the control action at time t1 based on the current state and past state at time t0

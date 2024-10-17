b18101d2c68dfe96d300a3082fbbde117801171d
nktt12 <nikhil.karun.cis@gmail.com>
2024-10-17 21:26:48 +0000

    Applying the controller equation

--- uuv_mission/controllerPD.py
+++ uuv_mission/controllerPD.py
@@ -3,5 +3,12 @@ class PDController:
         #Initialisation the controller variables
         self.Kp=Kp
         self.Kd=Kd
-        self.previous_error=0
         #to prevent error in the proprotional derivative term
+        self.previous_error=0
+    def controller_action(self, error: float) -> float:
+        #Using the preliminary controller design equation
+        difference = error - self.previous_error
+        output = self.Kp * error + self.Kd * difference
+        #Update the error term
+        self.previous_error = error
+        return output

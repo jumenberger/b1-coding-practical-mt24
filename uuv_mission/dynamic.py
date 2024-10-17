c5d7b697786dec9db8087d533a49b6766c68d18e
nktt12 <nikhil.karun.cis@gmail.com>
2024-10-17 10:38:22 +0000

    Using correct variable

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -77,7 +77,7 @@ class Mission:
     @classmethod
     def from_csv(cls, file_name: str):
         # You are required to implement this method
-        mission_data = pd.read_csv('mission.csv')
+        mission_data = pd.read_csv(file_name)
         pass
 
 

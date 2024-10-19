c9f641b5bf11fa3f2f3fd11f3ee92b84b7693094
Navik <navikbandara16@gmail.com>
2024-10-19 22:19:23 +0100

    implemented control

---
+++ uuv_mission/control.py
@@ -0,0 +1,11 @@
+class Controller:
+    def __init__(self, Kp, Kd):      #initialise PD controller with gains and error
+        self.Kp = Kp
+        self.Kd = Kd
+        self.previous_error = 0
+
+    def compute(self, error):           #calculate control action based on current error
+        derivative_error = error - self.previous_error
+        control_action = self.Kp * error + self.Kd * derivative_error
+        self.previous_error = error
+        return control_action

08c469075f059ff4dbe109135e3c476447c15467
jsoep <jsoepadmo@hotmail.sg>
2024-10-21 21:16:26 +0100

    fixed controller

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,11 +1,12 @@
 class Controller:
     def __init__(self):
-        pass
+        self.Kp = 0.15
+        self.Kd = 0.6
+        self.prev_error = 0
 
-    def get_action(self, mission, t, observation_t):
+    def get_action(self, reference_t, observation_t):
         # You are required to implement this method
-        Kp = 0.15
-        Kd = 0.6
-        error = mission.reference[t] - observation_t
-        error_dot = mission.reference[t] - mission.reference[t-1]
-        return Kp * error + Kd * error_dot
+        error = reference_t - observation_t
+        error_dot = error - self.prev_error
+        self.prev_error = error
+        return self.Kp * error + self.Kd * error_dot
--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -104,7 +104,7 @@ class ClosedLoop:
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
             # Call your controller here
-            actions[t] = self.controller.get_action(mission, t, observation_t)
+            actions[t] = self.controller.get_action(mission.reference[t], observation_t)
             self.plant.transition(actions[t], disturbances[t])
 
         return Trajectory(positions)
